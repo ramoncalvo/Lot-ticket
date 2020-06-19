50ebb5afdc68625ea9e2cdf79a0b9ed610ae1f63
Ramon Calvo <ramon.calvo@gmail.com>
2020-06-18 21:24:36 -0500

    stable version

--- src/App.js
+++ src/App.js
@@ -1,82 +1,69 @@
-import React from 'react';
-import Boleto from './assets/boletoPng';
+import React from "react";
+import Boleto from "./assets/boletoPng";
 import Pdf from "react-to-pdf";
-import './App.css';
-import users from './assets/participantes'
+import "./App.css";
+import users from "./assets/participantes";
 const ref = React.createRef();
 
-
-// const users = [
-//   {
-//     name: "1 Rigoberto Sanchez",
-//     lastname: "",
-//     numero: "5854"
-//   },
-//   {
-//     name: "2 Pedro Sola",
-//     lastname: "",
-//     numero: "9526"
-//   },
-//   {
-//     name: "3 Rosa VillaNueva",
-//     lastname: "",
-//     numero: "84525"
-//   }
- 
-// ];
-
 const options = {
-    orientation: 'landscape',
-    
+  orientation: "landscape",
 };
 
 function App() {
   let [folio, setFolio] = React.useState(0);
-  let suma = 0
+  let suma = 0;
 
   function startPdf() {
-    
     // console.log('numero de elementos ', users.length)
-    if(suma < users.length) {
+    if (suma < users.length) {
       var myVar = setInterval(myTimer, 3000);
-      
-      console.log('folio is ', suma)
+
+      console.log("folio is ", suma);
     } else {
       clearInterval(myVar);
     }
-        
   }
 
   function myTimer() {
-    
-    if(suma < users.length) {
-      var elem = document.getElementById('generate');
+    if (suma < users.length) {
+      var elem = document.getElementById("generate");
       elem.click();
-       suma++
-      setFolio( suma )
-      console.log('folio ', suma)
+      suma++;
+      setFolio(suma);
+      console.log("folio ", suma);
     }
-   
   }
 
   return (
-    <div className="App">
-       <div className="App">
-       <div>
-        <button onClick={ () => startPdf() }>Start PDF process</button>
-       </div>
-      <Pdf targetRef={ref} options={options} filename={ 'numero_' +folio + ".pdf"}>
-        {({ toPdf }) => <button id="generate" onClick={toPdf}>Generate Pdf</button>}
-        
-      </Pdf>
-      <div ref={ref} className="boletoWrapped">
+    <div >
+      <div className="App">
 
-        { folio < users.length ?
-          <Boleto name={users[folio].nombre} numero={users[folio].numero} />
-          : 'Se ha terminado'
-        }
+        <div className="ui">
+            <button className="btn" onClick={() => startPdf()}>Generar pds por lote</button>
+          
+            <Pdf
+              targetRef={ref}
+              options={options}
+              filename={"numero_" + folio + ".pdf"}
+            >
+              {({ toPdf }) => (
+                <button id="generate" onClick={toPdf}>
+                  Generar actual pdf
+                </button>
+              )}
+            </Pdf>
+          </div>
+        
+        <div ref={ref} className="boletoWrapped">
+          {folio < users.length ? (
+            <Boleto name={users[folio].nombre} numero={users[folio].numero} />
+          ) : (
+            "Se ha terminado"
+          )}
+        </div>
       </div>
-    </div>
+
+      
     </div>
   );
 }
